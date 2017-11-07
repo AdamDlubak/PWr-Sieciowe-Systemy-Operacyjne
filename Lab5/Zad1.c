265f97995005a24e5bb77326db3569b53209ded1
lstudent <lstudent@mail.pwr.wroc.pl>
2017-11-07 13:25:10 +0100

    Working task 1

--- Lab5/Zad1.c
+++ Lab5/Zad1.c
@@ -12,44 +12,57 @@
 	pthread_mutex_t mutex = PTHREAD_MUTEX_INITIALIZER;
 	pthread_cond_t cond   = PTHREAD_COND_INITIALIZER;
 
-void *makeDeposit(void *deposit) {
+	struct depositParameters {
+		int account;
+		int value;
+	};
+int randomInt(int min, int max) {
+    int tmp;
+    if (max>=min) max-= min;
+    else {
+        tmp= min - max;
+        min= max;
+        max= tmp;
+    }
+    return max ? (rand() % max + min) : min;
+}
+
+void *makeDeposit(void *depParam) {
+
+		struct depositParameters *param = (struct depositParameters*)depParam;
 
 		/* Entering to Critical Section */
         int result = pthread_mutex_lock(&mutex);
         	if (result != 0) { perror("Error pthread_mutex_lock\n"); exit(0); } 
 
-		while (semAccount[0] <= 0) {
+		while (semAccount[(*param).account] <= 0) {
 			pthread_cond_wait(&cond, &mutex);
 		}
-		semAccount[0]--;
+		semAccount[(*param).account]--;
 
 		result = pthread_mutex_unlock(&mutex);
         	if (result != 0) { perror("Error pthread_mutex_unlock\n"); exit(0); } 
 
 		
 		/* Critical Section operation */
-		int dep = *((int *)deposit);
-		int tmp = bankBalance[0];
-		tmp += dep;
-		sleep(2);
-		bankBalance[0] = tmp;
-		printf("Wynik mojej operacji %d\n", bankBalance[0]);
+
+		int tmp = bankBalance[(*param).account];
+		tmp += (*param).value;
+		sleep(1);
+		bankBalance[(*param).account] = tmp;
+		printf("Wynik mojej operacji (konto %d) po wplacie %d wynosi %d\n", (*param).account, (*param).value, bankBalance[(*param).account]);
 
 
 		/* Escaping from Critical Section */
         result = pthread_mutex_lock(&mutex);
         	if (result != 0) { perror("Error pthread_mutex_lock\n"); exit(0); } 
-		
-		semAccount[0]++;
+		semAccount[(*param).account]++;
 		pthread_cond_signal(&cond);
 		
 		result = pthread_mutex_unlock(&mutex);
         	if (result != 0) { perror("Error pthread_mutex_unlock\n"); exit(0); } 
-      
-		/* Return result of function */
-		int *resultOfFunction = malloc(sizeof(int));
-		*resultOfFunction = 1;
-		return resultOfFunction;
+   		free(depParam);   
+		return NULL;
 }
 
 /* 
@@ -61,16 +74,23 @@ void *makeDeposit(void *deposit) {
 int main(int argc, char * argv[]){ 
 
 	/* Running parameters */
+    struct depositParameters *depParam;
     int i, result, threadsAmt = 0;
-	int threadsArray[] = { 3, 2, 1, 2}; 						/* Number of particular operations */
-	int values[] = { 100, 100, 200, 500, 100, 250 };			/* Values of deposits */	
+	int threadsArray[4] = { 3, 2, 1, 2}; 						/* Number of particular operations */
+	int values[6] = { 100, 150, 200, 500, 130, 250 };			/* Values of deposits */	
 	for(i = 0; i < 4; i++) { threadsAmt += threadsArray[i]; }
     pthread_t bankThread[threadsAmt]; 							/* General number of threads */
 	
 
 	for(i = 0; i < threadsAmt; i++) {
+
+		/* Preparing datas */
+		depParam = malloc(sizeof(struct depositParameters));
+		(*depParam).account = randomInt(0, 2);
+		(*depParam).value = values[randomInt(0, 6)];
+
 		/* Creating threads */
-		result = pthread_create(&bankThread[i], NULL, makeDeposit, &values[0]);
+		result = pthread_create(&bankThread[i], NULL, makeDeposit, (void*) depParam);
 		if (result != 0) {
 			perror("Error pthread_create() - Could not create thread!\n");
 		}
@@ -82,9 +102,6 @@ int main(int argc, char * argv[]){
     	result = pthread_join(bankThread[i], (void **) &bankThreadResult);
 		if( result != 0) {
 			perror("Error pthread_join() - Could not join thread!\n");
-		}
-		else {
-			printf("Thread returned, %d.\n", *bankThreadResult);
 		}
 	}
 
